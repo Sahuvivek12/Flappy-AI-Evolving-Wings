b0a9d9d0d42bf772c0f1264b4ce6bc96d10e08f1
Vivek sahu <viveksahu818@gmail.com>
2024-08-31 15:27:35 +0530

    Update Flappy_Bird.py
    optimized the code
    tried to make the bird animation 60fps
    tried to improve fitness logic

--- Flappy_Bird.py
+++ Flappy_Bird.py
@@ -1,6 +1,5 @@
-import neat.population
-import pygame
 import neat
+import pygame
 import time
 import os
 import random
@@ -11,7 +10,7 @@ WIN_WIDTH = 500
 WIN_HEIGHT = 800
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT), pygame.HWSURFACE | pygame.DOUBLEBUF)
 pygame.display.set_caption("Flappy Bird")
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "bird1.png", ))), pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "bird3.png")))]
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "bird3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("Flappy bird with AI/imgs", "bg.png")).convert_alpha())
@@ -21,7 +20,7 @@ STAT_FONT = pygame.font.SysFont('Comic Sans MS', 30)
 class FlappyBird:
     IMGS = BIRD_IMGS
     MAX_ROTATION = 25
-    ROT_VEL = 20
+    ROT_VEL = 16
     ANIMATION_TIME = 5
 
     def get_rect(self):
@@ -38,50 +37,53 @@ class FlappyBird:
         self.img = self.IMGS[0]
 
     def jump(self):
-        self.velocity = -10.5
+        self.velocity = -10
         self.tick_count = 0
         self.height = self.y
 
     def move(self):
         self.tick_count += 1
-        d = self.velocity*self.tick_count + 1.5*self.tick_count**2
+        d = self.velocity * self.tick_count + 1.5 * self.tick_count ** 2
 
-        if d>=8:
-            d = 8
-        if d<0:
+        if d >= 6:
+            d = 6
+        if d < 0:
             d -= 2
 
         self.y = self.y + d
 
-        if d<0 or self.y < self.height + 50:
+        if d < 0 or self.y < self.height + 30:
             if self.tilt < self.MAX_ROTATION:
                 self.tilt = self.MAX_ROTATION
         else:
             if self.tilt > -90:
                 self.tilt -= self.ROT_VEL
 
-
     def draw(self, win):
         self.img_count += 1
 
         if self.img_count < self.ANIMATION_TIME:
             self.img = self.IMGS[0]
-        elif self.img_count < self.ANIMATION_TIME*2:
+        elif self.img_count < self.ANIMATION_TIME * 2:
             self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME*3:
+        elif self.img_count < self.ANIMATION_TIME * 3:
             self.img = self.IMGS[2]
-        elif self.img_count < self.ANIMATION_TIME*4:
+        elif self.img_count < self.ANIMATION_TIME * 4:
             self.img = self.IMGS[1]
-        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+        elif self.img_count < self.ANIMATION_TIME * 5:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME * 6:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME * 6:
             self.img = self.IMGS[0]
             self.img_count = 0
 
         if self.tilt <= -80:
             self.img = self.IMGS[1]
-            self.img_count = self.ANIMATION_TIME*2
+            self.img_count = self.ANIMATION_TIME * 2
 
         rotated_image = pygame.transform.rotate(self.img, self.tilt)
-        new_rectangle = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
+        new_rectangle = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
         win.blit(rotated_image, new_rectangle.topleft)
 
     def get_mask(self):
@@ -89,19 +91,16 @@ class FlappyBird:
     
 class Pipe:
     GAP = 200
-    VEL = 3.5
-    
+    VEL = 3
 
     def __init__(self, x):
         self.x = x
         self.height = 0
         self.gap = 100
-
         self.top = 0
         self.bottom = 0
         self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
         self.PIPE_BOTTOM = PIPE_IMG
-
         self.passed = False
         self.set_height()
 
@@ -130,29 +129,24 @@ class Pipe:
             top_offset = (self.x - bird.x, self.top - round(bird.y))
             bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
 
-            t_point = bird_mask.overlap(top_mask, top_offset)
-            b_point = bird_mask.overlap(bottom_mask, bottom_offset)
-
-            if t_point or b_point:
+            if bird_mask.overlap(top_mask, top_offset) or bird_mask.overlap(bottom_mask, bottom_offset):
                 return True
+
         return False
+
 class Base:
-    VEL = 5
+    VEL = 3.5
     WIDTH = BASE_IMG.get_width()
     IMG = BASE_IMG
-
 
     def __init__(self, y):
         self.y = y
         self.x1 = 0
         self.x2 = self.WIDTH
 
-        
     def move(self):
         self.x1 -= self.VEL
         self.x2 -= self.VEL
-
-
         if self.x1 + self.WIDTH < 0:
             self.x1 = self.x2 + self.WIDTH
         if self.x2 + self.WIDTH < 0:
@@ -162,18 +156,16 @@ class Base:
         win.blit(self.IMG, (self.x1, self.y))
         win.blit(self.IMG, (self.x2, self.y))
 
-
 def draw_window(win, birds, pipes, base, score, phase):
     win.blit(BG_IMG, (0, 0))
-
 
     for pipe in pipes:
         pipe.draw(win)
 
-    text = STAT_FONT.render('Score: '+ str(score), 1,(255, 255, 255))
+    text = STAT_FONT.render('Score: ' + str(score), 1, (255, 255, 255))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
 
-    text = STAT_FONT.render('Phase: '+ str(PHASE), 1,(255, 255, 255))
+    text = STAT_FONT.render('Phase: ' + str(PHASE), 1, (255, 255, 255))
     win.blit(text, (10, 10))
     
     base.draw(win)
@@ -182,7 +174,6 @@ def draw_window(win, birds, pipes, base, score, phase):
         bird.draw(win)
 
     pygame.display.update()
-             
 
 def main(genomes, config):
     global PHASE
@@ -198,7 +189,7 @@ def main(genomes, config):
         GE.append(g)
 
     base = Base(730)
-    pipes = [Pipe(700)]
+    pipes = [Pipe(600)]
     clock = pygame.time.Clock()
 
     score = 0
@@ -216,7 +207,7 @@ def main(genomes, config):
 
         pipe_ind = 0
         if len(flappy_birds) > 0:
-            if len(pipes)>1 and flappy_birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+            if len(pipes) > 1 and flappy_birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
                 pipe_ind = 1
         else:
             run = False
@@ -233,15 +224,12 @@ def main(genomes, config):
         add_pipe = False
         rem = []
         for pipe in pipes:
-            for flappy_bird in flappy_birds:
-                for x, bird in enumerate(flappy_birds):
-                    if pipe.collide(flappy_bird):
-                        GE[x].fitness = -1
-                        flappy_birds.pop(x)
-                        nets.pop(x)
-                        GE.pop(x)
-
-                    
+            for x, flappy_bird in enumerate(flappy_birds):
+                if pipe.collide(flappy_bird):
+                    GE[x].fitness = -1
+                    flappy_birds.pop(x)
+                    nets.pop(x)
+                    GE.pop(x)
 
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 rem.append(pipe)
@@ -263,10 +251,9 @@ def main(genomes, config):
                 flappy_birds.pop(x)
                 nets.pop(x)
                 GE.pop(x)
-                
+
         base.move()
         draw_window(WIN, flappy_birds, pipes, base, score, PHASE)
-            
 
 def Run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
@@ -278,11 +265,9 @@ def Run(config_path):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
-    winner = p.run(main,30)
-
+    winner = p.run(main, 30)
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-NEAT.txt')
     Run(config_path)
-
